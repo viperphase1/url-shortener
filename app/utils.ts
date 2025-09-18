fb80efb0ef3f8b5563f59cbd0181a09c0cece49c
viperphase1 <thomasgriffiths11@hotmail.com>
2025-09-18 13:29:47 -0600

    Remove unused code

--- app/utils.ts
+++ app/utils.ts
@@ -1,17 +1,3 @@
-export async function hashString(inputString: string) {
-    const textEncoder = new TextEncoder();
-    const data = textEncoder.encode(inputString); // Encode the string to a Uint8Array
-
-    // Use SHA-256 for a 256-bit (64-character hex) fixed-length hash
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-
-    // Convert the ArrayBuffer to a hexadecimal string
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hexHash = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-
-    return hexHash;
-}
-
 // asynchronous version of string replace so we can look for hashes in the db in parallel
 export async function replaceAsync(str: string, regex: RegExp, asyncFn: Function) {
     const promises: any = [];
@@ -24,14 +10,4 @@ export async function replaceAsync(str: string, regex: RegExp, asyncFn: Function
     let i = 0;
 
     return str.replace(regex, () => data[i++]);
-}
-
-export function getHost(req: any) {
-    const host = req.headers.host; // e.g., "example.com"
-
-    // Determine protocol (Vercel sets 'x-forwarded-proto')
-    const protocol = req.headers["x-forwarded-proto"] || "http";
-
-    const fullHost = `${protocol}://${host}`;
-    return fullHost;
 }
