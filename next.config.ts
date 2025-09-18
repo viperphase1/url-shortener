d5234dd52470081c076313b5ea56bd9d387582da
viperphase1 <thomasgriffiths11@hotmail.com>
2025-09-17 22:37:15 -0600

    Comment out rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     // forward all unmatched routes to the redirect function so we don't need a second path fragment on shortened urls
-    async rewrites() {
-        return [
-            {
-                source: '/:id*',
-                destination: '/api/:id*',
-            },
-        ];
-    }
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/:id*',
+    //             destination: '/api/:id*',
+    //         },
+    //     ];
+    // }
 };
 
 export default nextConfig;
